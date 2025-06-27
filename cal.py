7462433575dc6e4376b9cc7a046c9b4912259fdb
Rimi Chakraborty <143891192+rimichak@users.noreply.github.com>
2025-06-27 09:39:13 +0000

    added code

--- cal.py
+++ cal.py
@@ -1,8 +1,26 @@
-def add():
-    pass
-def sub():
-    pass
-def mul():
-    pass
-def div():
-    pass
+def add(a , b):
+    return a + b
+def sub(a , b):
+    return a - b
+def mul(a , b):
+    return a * b
+def div(a , b):
+    return a / b
+print(" select your operations:\n",
+"1. Add\n", "2. Subtract\n", "3. Multiplication\n", "4. Division\n")
+operations = int(input("Enter your operation number:"))
+a = int(input("Enter your number:"))
+b = int(input("Enter your number:"))
+if operations == 1:
+    print("Addition:", add(a, b))
+elif operations == 2:
+    print("Subtraction:", sub(a,b))
+elif operations == 3:
+    print("multiplication:", mul(a,b))
+elif operations == 4:
+    print("Division:", div(a,b))
+else:
+    print("invalid operations")
+
+
+
